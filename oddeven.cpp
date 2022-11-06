fc43da001a5066fd9a6bfa006bc37f022f39cbe4
Md. Tusher Ahmad Bappy <106505665+tusherahmadbappy@users.noreply.github.com>
2022-11-06 15:24:06 +0600

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout<<"Enter your any number: ";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<num<<" is Even Number "<<endl;
+    }
+    else
+    {
+        cout<<num<<" is odd Number "<<endl;
+    }
+    return 0;
+}
